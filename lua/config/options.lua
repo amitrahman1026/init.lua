c2fc7a0d21ee49880d6064632b76fe87e86f0664
amitrahman1026 <amit.amit.rahman@gmail.com>
2024-12-19 22:57:50 +0800

    python path on my box

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -47,7 +47,7 @@ opt.updatetime = 50 -- Faster completion
 opt.colorcolumn = "80" -- Show a vertical line at column 80
 
 -- Python provider for some plugins
-vim.g.python3_host_prog = "/opt/homebrew/bin/python3"
+vim.g.python3_host_prog = "/usr/bin/python3"
 
 -- Netrw tree mode
 -- vim.g.netrw_liststyle = 3
