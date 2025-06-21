0d3ec538b8757d307db22f66d55a1fb2f768a3ce
amitrahman1026 <amit.amit.rahman@gmail.com>
2025-06-22 01:04:37 +0800

    upgrate to native lsp nvim 0.11

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,26 +1,13 @@
+-- LSP Simplified for Neovim 0.11 native LSP
 return {
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v4.x",
-		lazy = true,
-		config = false,
-		init = function()
-			-- Disable automatic setup, we are doing it manually
-			vim.g.lsp_zero_extend_cmp = 0
-			vim.g.lsp_zero_extend_lspconfig = 0
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = true,
-	},
-	-- Autocompletion
+	-- nvim-cmp for the superior completion experience
 	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
 		dependencies = {
-			{ "L3MON4D3/LuaSnip" },
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
 		},
 		config = function()
 			local cmp = require("cmp")
@@ -29,131 +16,101 @@ return {
 				sources = {
 					{ name = "path" },
 					{ name = "nvim_lsp" },
-					{ name = "nvim_lua" },
 					{ name = "buffer", keyword_length = 3 },
-					{ name = "luasnip", keyword_length = 2 },
-				},
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-p>"] = cmp.mapping.select_prev_item(),
 					["<C-n>"] = cmp.mapping.select_next_item(),
-					-- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-u>"] = cmp.mapping.scroll_docs(-4),
 					["<C-d>"] = cmp.mapping.scroll_docs(4),
 					["<Tab>"] = cmp.mapping.confirm({ select = true }),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Not sure how I feel about this one
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
+				completion = {
+					completeopt = "menu,menuone,noselect", -- don't auto-select
+				},
 			})
 		end,
 	},
-	-- LSP
 	{
 		"neovim/nvim-lspconfig",
-		cmd = { "LspInfo", "LspInstall", "LspStart" },
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "williamboman/mason-lspconfig.nvim" },
+			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
-			local lsp_zero = require("lsp-zero")
-			local lspconfig = require("lspconfig")
-
-			-- virtual text for errors became disabled by defaults in neovim 0.11
+			-- diagnostics config
 			vim.diagnostic.config({
 				virtual_text = true,
+                virtual_lines = false,
 				signs = true,
 				underline = true,
 				update_in_insert = false,
 				severity_sort = true,
 			})
 
-			local lsp_attach = function(client, bufnr)
-				local opts = { buffer = bufnr, remap = false }
-
-				vim.keymap.set("n", "gd", function()
-					vim.lsp.buf.definition()
-				end, opts)
-				vim.keymap.set("n", "K", function()
-					vim.lsp.buf.hover()
-				end, opts)
-				vim.keymap.set("n", "<leader>vws", function()
-					vim.lsp.buf.workspace_symbol()
-				end, opts)
-				vim.keymap.set("n", "<leader>vd", function()
-					vim.diagnostic.open_float()
-				end, opts)
-				vim.keymap.set("n", "[d", function()
-					vim.diagnostic.goto_next()
-				end, opts)
-				vim.keymap.set("n", "]d", function()
-					vim.diagnostic.goto_prev()
-				end, opts)
-				vim.keymap.set("n", "<leader>vca", function()
-					vim.lsp.buf.code_action()
-				end, opts)
-				vim.keymap.set("n", "<leader>vrr", function()
-					vim.lsp.buf.references()
-				end, opts)
-				vim.keymap.set("n", "<leader>vrn", function()
-					vim.lsp.buf.rename()
-				end, opts)
-				vim.keymap.set("i", "<C-h>", function()
-					vim.lsp.buf.signature_help()
-				end, opts)
-			end
-
-			lsp_zero.extend_lspconfig({
-				capabilities = require("cmp_nvim_lsp").default_capabilities(),
-				on_attach = lsp_attach,
-				float_border = "rounded",
-				sign_text = true,
-			})
-
-			require("mason-lspconfig").setup({
-				handlers = {
-					lsp_zero.default_setup,
-					lua_ls = function()
-						local lua_opts = lsp_zero.nvim_lua_ls()
-						lspconfig.lua_ls.setup(lua_opts)
-					end,
+			-- LSP servers using the new native config in nvim 0.11
+			vim.lsp.config.lua_ls = {
+				cmd = { "lua-language-server" },
+				root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "selene.toml" },
+				filetypes = { "lua" },
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						workspace = {
+							checkThirdParty = false,
+							library = { vim.env.VIMRUNTIME },
+						},
+					},
 				},
-			})
+			}
 
-			-- Specific setup for ocamllsp
-			lspconfig.ocamllsp.setup({
+			vim.lsp.config.clangd = {
+				cmd = { "/opt/homebrew/opt/llvm/bin/clangd", "--clang-tidy" },
+				root_markers = { "compile_commands.json", "compile_flags.txt", ".clangd", ".git" },
+				filetypes = { "c", "cpp", "objc", "objcpp" },
+			}
+
+			vim.lsp.config.ocamllsp = {
 				cmd = { "opam", "exec", "--", "ocamllsp" },
-				capabilities = require("cmp_nvim_lsp").default_capabilities(),
-				filetypes = {
-					"ocaml",
-					"ocaml.menhir",
-					"ocaml.interface",
-					"ocaml.ocamllex",
-					"reason",
-					"dune",
-				},
-				root_dir = lspconfig.util.root_pattern(
-					"*.opam",
-					"esy.json",
-					"package.json",
-					".git",
-					"dune-project",
-					"dune-workspace"
-				),
-				on_attach = lsp_attach,
+				root_markers = { "*.opam", "esy.json", "package.json", ".git", "dune-project", "dune-workspace" },
+				filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
+			}
+
+			vim.lsp.config.rust_analyzer = {
+				cmd = { "rust-analyzer" },
+				root_markers = { "Cargo.toml", "rust-project.json" },
+				filetypes = { "rust" },
+			}
+
+			-- enable lsp servers here
+			vim.lsp.enable({ "lua_ls", "clangd", "ocamllsp", "rust_analyzer" })
+
+			-- DON'T enable native completion since we're using nvim-cmp
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(ev)
+					-- custom keymaps
+					local opts = { buffer = ev.buf, remap = false }
+
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+					vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+					vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+					vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+					vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+					vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+				end,
 			})
 
-			-- Specific setup for clangd
-			lspconfig.clangd.setup({
-				cmd = { "/opt/homebrew/opt/llvm/bin/clangd", "--clang-tidy" }, -- Use homebrew's llvm/clangd ideally, remember to add the appropriate alias' on a mac
-				capabilities = require("cmp_nvim_lsp").default_capabilities(),
-				on_attach = lsp_attach,
-			})
+            -- TODO: anything else?
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			-- set window borders
+			vim.o.winborder = "rounded"
 		end,
 	},
 }
